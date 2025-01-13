4376329949d5c4f14245d91864d4bee5e68a628c
Mohsin Pannu <mohsin.pannu170@gamil.com>
2025-01-13 05:33:48 +0500

    Add cabin creation form with validation and integrate Supabase for data handling

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -5,6 +5,10 @@ import Form from "../../ui/Form";
 import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
+import { useForm } from "react-hook-form";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createCabin } from "../../services/apiCabins";
+import toast from "react-hot-toast";
 
 const FormRow = styled.div`
   display: grid;
@@ -43,36 +47,87 @@ const Error = styled.span`
 `;
 
 function CreateCabinForm() {
+  const { register, handleSubmit, reset, getValues, formState } = useForm()
+  
+  const { errors } = formState
+  console.log(errors)
+
+  const queryClient = useQueryClient()
+
+  const {mutate, isLoading: isCreating} = useMutation({
+    mutationFn: createCabin,
+    onSuccess: () => {
+      toast.success('Cabin created')
+      queryClient.invalidateQueries({ queryKey: 'cabins' })
+      reset()
+    },
+    onError: () => {
+      toast.error('Cabin could not be')
+    }
+  })
+
+
+  function onSubmit(data) {
+    mutate(data)
+  }
+
+  function onError(errors) {
+    console.log(errors)
+  }
+
   return (
-    <Form>
+    <Form onSubmit={handleSubmit(onSubmit, onError)}>
       <FormRow>
         <Label htmlFor="name">Cabin name</Label>
-        <Input type="text" id="name" />
+        <Input type="text" id="name" {...register('name', {
+          required: 'Cabin name is required'
+        })} />
+        {errors?.name?.message && <Error>{errors.name.message}</Error>}
       </FormRow>
 
       <FormRow>
         <Label htmlFor="maxCapacity">Maximum capacity</Label>
-        <Input type="number" id="maxCapacity" />
+        <Input type="number" id="maxCapacity"  {...register('maxCapacity', {
+          required: 'Cabin capacity is required',
+          min: {
+            value: 1,
+            message: 'Capacity must be at least 1'
+          }
+        })}/>
+        {errors?.maxCapacity?.message && <Error>{errors.maxCapacity.message}</Error>}
       </FormRow>
 
       <FormRow>
         <Label htmlFor="regularPrice">Regular price</Label>
-        <Input type="number" id="regularPrice" />
+        <Input type="number" id="regularPrice" {...register('regularPrice', {
+          required: 'Cabin price is required',
+          min: {
+            value: 1,
+            message: 'Capacity must be at least 1'
+          }
+        })} />
+        {errors?.regularPrice?.message && <Error>{errors.regularPrice.message}</Error>}
       </FormRow>
 
       <FormRow>
         <Label htmlFor="discount">Discount</Label>
-        <Input type="number" id="discount" defaultValue={0} />
+        <Input type="number" id="discount" defaultValue={0} {...register('discount', {
+          required: 'Cabin discount is required',
+          validate: (value) => value < getValues().regularPrice || 'Discount must be less than regular price'
+        })} />
+        {errors?.discount?.message && <Error>{errors.discount.message}</Error>}
       </FormRow>
 
       <FormRow>
         <Label htmlFor="description">Description for website</Label>
-        <Textarea type="number" id="description" defaultValue="" />
+        <Textarea type="number" id="description" defaultValue="" {...register('description', {
+          required: 'Cabin description is required',
+        })} />
       </FormRow>
 
       <FormRow>
         <Label htmlFor="image">Cabin photo</Label>
-        <FileInput id="image" accept="image/*" />
+        <FileInput id="image" accept="image/*"  />
       </FormRow>
 
       <FormRow>
@@ -80,7 +135,7 @@ function CreateCabinForm() {
         <Button variation="secondary" type="reset">
           Cancel
         </Button>
-        <Button>Edit cabin</Button>
+        <Button disabled={isCreating}>Add cabin</Button>
       </FormRow>
     </Form>
   );
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -11,6 +11,17 @@ export async function getCabins() {
   return data
 }
 
+export async function createCabin(cabin) {
+  const { data, error } = await supabase.from('cabins').insert([cabin])
+
+  if (error) {
+    console.log(error)
+    throw new Error('Cabin could not be created.')
+  }
+
+  return data
+}
+
 export async function deleteCabin(id) {
   const { data, error } = await supabase.from('cabins').delete().eq('id', id)
 
